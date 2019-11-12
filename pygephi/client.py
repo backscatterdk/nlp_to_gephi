78ac953997154ab823855aaba7301e4a6faef456
Bertil Johannes Ipsen <36302567+bipsen@users.noreply.github.com>
2019-11-12 11:54:48 +0100

    Update client.py

--- pygephi/client.py
+++ pygephi/client.py
@@ -15,6 +15,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This file has been reworked by Bertil Ipsen.
 
 """
 Allow a Python script to communicate with Gephi using the Gephi Graph Streaming protocol and plugin.
