693a775608b31d29527056de64e47db15b900c54
Bertil <ipsen.bertil@gmail.com>
2019-11-21 18:57:02 +0100

    Changed the gform file to actually work with gephi

--- form-to-gephi.py
+++ form-to-gephi.py
@@ -1,23 +1,32 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
+from nlp_to_net import start_nlp, draw_nodes
 import time
 
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('form-to-gephi ... .json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('form-to-gephi-e2ab2e110481.json', scope)
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open("Some form (Responses)").sheet1
+wks = gc.open("New test form (Responses)").sheet1
 
+global_nlp = start_nlp()
+
+# Keep track of old responses
 old = []
+
 while True:
     sheet_values = wks.get_all_values()
+
+    # We only want the question column
     fresh = [lst[1] for lst in sheet_values[1:]]
+
+    # We only want to send the new values to gephi
     to_gephi = [text for text in fresh if text not in old]
-    print(to_gephi)
+    for text in to_gephi:
+        draw_nodes(text=text, nlp=global_nlp)
     old = fresh
-    time.sleep(10)
+    time.sleep(2)
